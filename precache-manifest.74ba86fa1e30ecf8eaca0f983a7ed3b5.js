d0ee677fce897e2bff31d2aba949e830baa3a3b5
laith <mr.laith_2011@hotmail.com>
2020-10-12 22:43:39 +0300

    Updates

---
+++ precache-manifest.74ba86fa1e30ecf8eaca0f983a7ed3b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763debec0b0259323c76725ae782c799",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "56de3a72d931af213093",
+    "url": "/resty/static/css/main.5018b05d.chunk.css"
+  },
+  {
+    "revision": "44e14fe12984f146a963",
+    "url": "/resty/static/js/2.607202a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.607202a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56de3a72d931af213093",
+    "url": "/resty/static/js/main.39d0d86e.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
